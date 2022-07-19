19e3ad93a6b1053c6c3e3a91a5f5115d015d7c4c
SouravBandyopadhyay <souravb.1998@gmail.com>
2022-07-20 00:51:59 +0530

    header push test

---
+++ header.js
@@ -0,0 +1,15 @@
+let navbar = ()=>{
+    `<img
+    id="logo"
+    src="https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd37c83dfa3ccb0d2d9836f_myhours-logo.svg"
+    alt=""
+  />
+  <div id="pages">
+    <p class="link"><a href="#">How It Works</a></p>
+    <p class="link"><a href="">Use Cases</a></p>
+    <p class="link"><a href="">Pricing</a></p>
+    <p class="link"><a href="">Support</a></p>
+    <p class="link"><a href="">Sign in</a></p>
+    <button id="btn1" >Get My Hours Free</button>
+  </div>`
+}
