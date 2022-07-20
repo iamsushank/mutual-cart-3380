fe3fbc7d0b38b1614083bbd86af73c2849917763
SouravBandyopadhyay <souravb.1998@gmail.com>
2022-07-21 00:25:32 +0530

    day-2 all task done

--- header.js
+++ header.js
@@ -1,16 +1,17 @@
 let navbar = ()=>{
-    return `<img
-    id="logo"
-    src="https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd37c83dfa3ccb0d2d9836f_myhours-logo.svg"
-    alt=""
-  />
-  <div id="pages">
-    <p class="link"><a href="#">How It Works</a></p>
-    <p class="link"><a href="">Use Cases</a></p>
-    <p class="link"><a href="">Pricing</a></p>
-    <p class="link"><a href="">Support</a></p>
-    <p class="link"><a href="">Sign in</a></p>
-    <button id="btn1" >Get My Hours Free</button>
+    return `      <div id="navbar">
+    <img
+      id="navlogo"
+      src="https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd37c83dfa3ccb0d2d9836f_myhours-logo.svg"
+      alt=""/>
+    <div id="navpages">
+      <p class="navlink"><a class="nav-a" href="#">How It Works</a></p>
+      <p class="navlink"><a class="nav-a" href="">Use Cases</a></p>
+      <p class="navlink"><a class="nav-a" href="">Pricing</a></p>
+      <p class="navlink"><a class="nav-a" href="">Support</a></p>
+      <p class="navlink"><a class="nav-a"href="">Sign in</a></p>
+      <button id="navbtn1" >Get My Hours Free</button>
+    </div>
   </div>`
 };
 export default navbar;
