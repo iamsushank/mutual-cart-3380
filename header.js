2ac76e9276785ab4ce5c11ae11c48021e091eb06
JyotiP123 <jyoti.pardhi05@gmail.com>
2022-07-21 11:31:37 +0530

    files

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 let navbar = ()=>{
-    return `      <div id="navbar">
+    return `<div id="navbar">
     <img
       id="navlogo"
       src="https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd37c83dfa3ccb0d2d9836f_myhours-logo.svg"
