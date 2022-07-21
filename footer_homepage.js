274d9b8c7d86b2ead2daac7be1a941e6d17e1cf9
Sushank <mandal678@gmail.com>
2022-07-21 11:59:07 +0530

    upodating repo

---
+++ footer_homepage.js
@@ -0,0 +1,81 @@
+let footer_home = () => {
+  return `
+  <div id="footer">
+  <div id="col-1">
+    <img 
+      src="https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc9236f9ab08c7bdf83fe75_Logo-300px.png"
+      alt=""
+    />
+    <p>© 2022 My Hours.</p>
+    <p>All rights reserved.</p>
+  </div>
+  <div class="cols">
+    <h2>Product</h2>
+    <a href="#"> How it works</a>
+    <br />
+    <a href="#">Features</a>
+    <br />
+    <a href="#">Mobile app</a>
+    <br />
+    <a href="#">Use case collection</a>
+    <br />
+    <a href="#">Pricing</a>
+    <br />
+    <a href="#">Guides</a>
+    <br />
+    <a href="#">Customer Reviews</a>
+    <br />
+    <a href="#">Start Free</a>
+  </div>
+  <div class="cols">
+    <h2>Resources</h2>
+    <a href="#"> Time Tracking Library</a>
+    <br />
+    <a href="#">About</a>
+    <br />
+    <a href="#">Terms of Use</a>
+    <br />
+    <a href="#">Sitemap</a>
+    <br />
+    <a href="#">Project Management Software</a>
+    <br />
+    <a href="#">Time Management Apps</a>
+    <br />
+    <a href="#">Best Time Tracking Apps of 2022</a>
+    <br />
+    <a href="#">Free Time Card Calculator</a>
+  </div>
+  <div class="cols">
+    <h2>Use Cases</h2>
+    <a href="#"> Project billing</a>
+    <br />
+    <a href="#">Time reports and Project analytics</a>
+    <br />
+    <a href="#">Time reports and Project analytics</a>
+    <br />
+    <a href="#">Expense tracking</a>
+    <br />
+    <a href="#">Calculating project profitability</a>
+    <br />
+    <a href="#">Timesheet time tracking</a>
+  </div>
+  <div class="cols">
+    <h2>Integrations</h2>
+    <a href="#"> QuickBooks</a>
+    <br />
+    <a href="#">Zapier</a>
+  </div>
+  <div class="cols">
+    <h2>Social</h2>
+    <a href="https://www.facebook.com/myHoursTeam/">Facebook</a>
+    <br />
+    <a href="https://twitter.com/MyHoursTeam">Twitter</a>
+  </div>
+</div>
+<div>
+  <p id="foot-line">
+    Looking for employee attendance and absence tracking? Visit <a href="https://allhours.com/?_ga=2.26538617.5131411.1658208313-1044255821.1658208313"><strong> All Hours.</strong></a> 
+  </p>
+</div>`;
+};
+export default footer_home;
