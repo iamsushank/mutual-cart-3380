5bbd69180f8301b9fbbeb938249f3c6ce0b76224
JyotiP123 <jyoti.pardhi05@gmail.com>
2022-07-23 12:23:24 +0530

    day5

--- team.js
+++ team.js
@@ -39,3 +39,33 @@ row.append(checkbox, name, activity, email, labor, bill, role, status, action);
 body.append(row);
 
 });
+
+
+let userinfoas = JSON.parse(localStorage.getItem("userDetails"));
+
+// setTimeout(() => {
+let mainaccordin = document.getElementsByClassName("conBoxes");
+
+for (i = 0; i < mainaccordin.length; i++) {
+  mainaccordin[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    //   this.style.color ="blue"|| "black"
+  });
+}
+// }, 2000);
+
+if (userinfoas === null) {
+  window.location.href = "../signup.html";
+} else {
+  let nameprofile = document.getElementById("name-profile");
+  nameprofile.innerText = userinfoas["fullname"];
+}
+
+let profile = () => {
+  window.location.href = "../profile.html";
+};
+
+let profileSignout = () => {
+  localStorage.removeItem("userDetails");
+  window.location.href = "../signup.html";
+};
