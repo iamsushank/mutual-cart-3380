1b76f0a5c61fb25cc7a4fe54cd62d678b8d864c0
JyotiP123 <jyoti.pardhi05@gmail.com>
2022-07-23 08:32:05 +0530

    day4 completed

--- team.js
+++ team.js
@@ -5,15 +5,22 @@ member_data.forEach(element => {
 
 let body = document.querySelector("tbody");
 let row = document.createElement("tr");
+row.style.fontSize="17px";
 let checkbox = document.createElement("td");
+
 let cb = document.createElement("input");
 cb.style.type = "checkbox";
 cb.setAttribute("class","cb2");
+
 let name = document.createElement("td");
 name.innerText = element.name;
+
 let activity = document.createElement("td");
+let act = document.createElement("a");
 activity.innerText = "View Activity";
-activity.style.color = "#0368ab"
+activity.style.cursor="pointer";
+activity.style.color = "#0368ab";
+
 let email = document.createElement("td");
 email.innerText = element.email;
 let labor = document.createElement("td");
@@ -27,7 +34,7 @@ status.innerText = "pending/completed";
 let action = document.createElement("td");
 action.innerText = "Delete";
 
-checkbox.append(cb);
+activity.append(act);
 row.append(checkbox, name, activity, email, labor, bill, role, status, action);
 body.append(row);
 
