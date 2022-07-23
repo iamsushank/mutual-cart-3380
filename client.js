d077c11ff374763c47cac9b4e635eeb92f225d11
Sushank Mandal <mandal678@gmail.com>
2022-07-23 17:25:30 +0530

    Revert "Merge pull request #34 from sushank678/fw19_1038_day-4"
    This reverts commit b19c64c14ac05976fe0600247bf02c10685485ad, reversing
    changes made to 2f8c3bc1e20dc74075423deb40a9b7480ea4eafd.

--- client.js
+++ client.js
@@ -1,108 +1,96 @@
-let arr = JSON.parse(localStorage.getItem("dataclient"))
+let arr= JSON.parse(localStorage.getItem("dataclient"))
+let data=document.getElementById("datacollect")
+data.innerHTML=""
+
+arr.forEach(function(ele,index){
+    let name1=document.createElement("td")
+    name1.innerText=ele.name
+name1.setAttribute("id","nameinfo")
+    let contact=document.createElement("td")
+    contact.innerText=ele.contact
+contact.setAttribute("id","contactinfo")
+    let email=document.createElement("td")
+    email.innerText=ele.email
+    email.setAttribute("id","emailinfo")
+
+    let active=document.createElement("td")
+    active.innerText="Active"
+    active.setAttribute("id","active_info")
 
 
-let getData = (arr) => {
+    let action=document.createElement("td")
+    action.setAttribute("id","actioninfo")
+    let iconji=document.createElement("i")
+    iconji.setAttribute("class","fa-solid fa-gear")
 
-    let data = document.getElementById("datacollect")
-    data.innerHTML = "" 
+let buttonji=document.createElement("select")
+var opt = document.createElement('option');
+opt.value = "";
+opt.innerHTML = "";
+buttonji.appendChild(opt);
+var opt = document.createElement('option');
+opt.value = "Edit";
+opt.innerHTML = "Edit";
+buttonji.appendChild(opt);
+var opt = document.createElement('option');
+opt.value = "Archieve";
+opt.innerHTML = "Archieve";
+buttonji.appendChild(opt);
+var opt = document.createElement('option');
+opt.value = "Delete";
+opt.innerHTML = "Delete";
+buttonji.appendChild(opt);
+buttonji.addEventListener("click",function(){
+    console.log(9)
+    deletedata(ele,index)
+})
+action.append(iconji,buttonji)
 
-    arr.forEach(function (ele, index) {
-        let name1 = document.createElement("td")
-        name1.innerText = ele.name
-        name1.setAttribute("id", "nameinfo")
-        let contact = document.createElement("td")
-        contact.innerText = ele.contact
-        contact.setAttribute("id", "contactinfo")
-        let email = document.createElement("td")
-        email.innerText = ele.email
-        email.setAttribute("id", "emailinfo")
+    let table_row=document.createElement("tr")
+    table_row.append(name1,contact,email,active,action)
 
-        let active = document.createElement("td")
-        active.innerText = "Active"
-        active.setAttribute("id", "active_info")
+let data=document.getElementById("datacollect")
+    data.append(table_row)
+    function deletedata(ele,index)
+{
+    
+console.log(opt.value)
+  
 
-
-        let action = document.createElement("td")
-        action.setAttribute("id", "actioninfo")
-        let iconji = document.createElement("i")
-        iconji.setAttribute("class", "fa-solid fa-gear")
-
-        let buttonji = document.createElement("select")
-        var opt = document.createElement('option');
-        opt.value = "";
-        opt.innerHTML = "";
-        buttonji.appendChild(opt);
-        var opt = document.createElement('option');
-        opt.value = "Edit";
-        opt.innerHTML = "Edit";
-        buttonji.appendChild(opt);
-        var opt = document.createElement('option');
-        opt.value = "Archieve";
-        opt.innerHTML = "Archieve";
-        buttonji.appendChild(opt);
-        var opt = document.createElement('option');
-        opt.value = "Delete";
-        opt.innerHTML = "Delete";
-        buttonji.appendChild(opt);
-        // buttonji.addEventListener("click", function () {
-        //     console.log(9)
-        //     deletedata(ele, index)
-        // })
-        action.append(iconji, buttonji)
-
-        let table_row = document.createElement("tr")
-        table_row.append(name1, contact, email, active, action)
-
-        let data = document.getElementById("datacollect")
-        data.append(table_row)
-    })
 }
-getData(arr);
-//         function deletedata(ele, index) {
-
-//             console.log(opt.value)
-
-
-//         }
-//     }
-
-
-
-// })
-
-let divji = document.getElementById("name-stng")
-divji.addEventListener("click", sorting)
-function sorting() {
-            //console.log(arr)
-            arr = arr.sort(function (a, b) {
-
-                if (a.name < b.name) {
-
-                    return -1
-                }
-                if (b.name > a.name) {
-                    return 1
-                }
-                return 0
-            })
-            //Window.location.reload()
-            console.log(arr)
-            localStorage.setItem("dataclient", JSON.stringify(arr))
-            // Window.location.reload();
-            getData(arr);
+let divji=document.getElementById("nameinfo")
+divji.addEventListener("click",sorting)
+function sorting()
+{
+    //console.log(arr)
+    arr= arr.sort(function(a,b){
+        
+if(a.name<b.name)
+{
+    
+    return -1
 }
+if(b.name>a.name)
+{
+    return 1
+}
+return 0
+})
+console.log(arr)
+localStorage.setItem("dataclient",JSON.stringify(arr))
+Window.location.reload()
+}
+})
 
-
-
-//     console.log(arr)
-// // let hji=[{name:"as"},{name:"hji"},{name:"bknfds"},{name:"ij"},{name:"cv"}]
-// let hji = [4, 5, 6, 7, 2, 3]
-// hji.sort(function (a, b) {
-//     return a - b
-
-// })
-// console.log(hji)
-
+console.log(arr)
+// let hji=[{name:"as"},{name:"hji"},{name:"bknfds"},{name:"ij"},{name:"cv"}]
+let hji=[4,5,6,7,2,3]
+hji.sort(function(a,b){
+   return a-b
+   
+})
+console.log(hji)
+ 
 
 
 
--- review.js
+++ review.js
@@ -1,8 +1,8 @@
 function homeji()
 {
-    console.log(8)
-    let arrayji=JSON.parse(localStorage.getItem("userDetails")) 
-    //console.log(Object.keys(arrayji).length)
+  console.log(8)
+  let arrayji=JSON.parse(localStorage.getItem("userDetails")) 
+  //console.log(Object.keys(arrayji).length)
 if(arrayji!==null)
 {
 window.location.href="./My_Hours/homepage.html"
