0041d1db0f5938de2460d3470b8344c447c131a4
Sushank Mandal <mandal678@gmail.com>
2022-07-24 11:34:51 +0530

    linking

--- header.js
+++ header.js
@@ -5,7 +5,7 @@ let navbar = () => {
       src="https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd37c83dfa3ccb0d2d9836f_myhours-logo.svg"
       alt=""/>
     <div id="navpages">
-      <p class="navlink"><a class="nav-a" href="./homep.html">How It Works</a></p>
+      <p class="navlink"><a class="nav-a" href="./index.html">How It Works</a></p>
       <p class="navlink"><a class="nav-a" href="./use.html">Use Cases</a></p>
       <p class="navlink"><a class="nav-a" href="./price.html">Pricing</a></p>
       <p class="navlink"><a class="nav-a" href="./supportpage.html">Support</a></p>
